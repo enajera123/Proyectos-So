0c840978be6450e8be596e470ce6340f22a4c0be
Ghost-Red <70116994+Ghost-Red@users.noreply.github.com>
2023-03-30 03:53:14 +0200

    Update Create.py

--- Proyecto 3/controlador/Create.py
+++ Proyecto 3/controlador/Create.py
@@ -6,8 +6,19 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 #		Controlador
 #=======================
 class Create(QMainWindow):
-    def __init__(self):#this
-      super(Create,self).__init__()#Inicializa la clase -> Initialize
-      uic.loadUi("vista/create.ui",self)
-      self.cbNombreUsuario.setCurrentIndex(-1)
-#Termina la clase
+  def __init__(self):#this
+    super(Create,self).__init__()#Inicializa la clase -> Initialize
+    uic.loadUi("vista/create.ui",self)
+    self.cbNombreUsuario.setCurrentIndex(-1)
+  def conectarEventos(self):
+    """Asocia los eventos con sus disparadores"""
+    self.btnAgregarUsuario.clicked.connect(self.btnAgregarUsuario_click)
+  # =======================
+  # Eventos
+  # =======================
+  
+  def btnAgregarUsuario_click(self, event):
+    Usuario = self.cbNombreUsuario.SelectedItem
+    if (self.cbNombreUsuario.SelectedIndex != -1):
+      self.listPermisosUsuarios.Items.Add(Usuario)
+  #Termina la clase
