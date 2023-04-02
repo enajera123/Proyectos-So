2ccb100926686ebb28f85c990acb066fed4f0623
Ghost-Red <70116994+Ghost-Red@users.noreply.github.com>
2023-04-02 11:29:28 -0600

    P3_Avanze modificar

--- Proyecto 3/controlador/modificar.py
+++ Proyecto 3/controlador/modificar.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QListWidget
+#from ManejoArchivo import ManejoArchivo
 
 #=======================
 #		Controlador
@@ -11,11 +12,22 @@ class Modificar(QMainWindow):
       uic.loadUi("vista/modificar.ui",self)
       self.cbNombreUsuario.setCurrentIndex(-1)
       self.btnRegresar.clicked.connect(self.btnRegresar_click)
-    
+      self.btnAgregarUsuario.clicked.connect(self.btnAgregarUsuario_click)
+      #self.ManejoArchivo_instance = ManejoArchivo()
+      #usuarios = self.ManejoArchivo_instance.leerUsuarios()
+      #self.cbNombreUsuario.addItems(usuarios)
+      
+      
     def btnRegresar_click(self,event):            
         # Notese que se importa el controlador en la funcion para evitar imports circulares
         from controlador.main import Main
         self.hide()
         self.nuevaVentana = Main()
-        self.nuevaVentana.show()  
+        self.nuevaVentana.show()
+        
+    def btnAgregarUsuario_click(self,event):            
+        #Agrega al listview el usuario con sus respectivos permisos
+        item = self.cbNombreUsuario.currentText()
+        if (self.cbNombreUsuario.currentIndex() != -1):
+          self.lisitWPerUsuarios.addItem(item)
 #Termina la clase
