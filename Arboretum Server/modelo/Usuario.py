2da46f90dcf7d9e1777255dc793a75e79626800a
Ghost-Red <70116994+Ghost-Red@users.noreply.github.com>
2023-05-22 09:50:37 -0600

    P1_Redes_Actualizacion clase Jugador

--- Arboretum Server/modelo/Usuario.py
+++ Arboretum Server/modelo/Usuario.py
@@ -1,38 +1,5 @@
-from Tablero import Tablero
-from Mazo import Mazo
 class Usuario:
     def __init__(self, nombre, ip, conexion):
         self.nombre = nombre
         self.ip = ip
         self.conexion = conexion
-        #Se le coloca todos por motivos practicos a la hora de enviarlos por la red
-        self.tablero = Tablero(nombre)
-        self.descartes = Mazo(nombre)
-        self.baraja = Mazo(nombre)
-        self.tiposArbolPuntuable = []
-        
-    def agregarArbolePuntuable(self,tiposArbol):
-        self.tiposArbolPuntuable.append(tiposArbol)
-        
-    def getNombre(self):
-        return self.nombre
-    
-    def setBaraja(self,cartas):
-        return self.baraja.setCartas(cartas)
-    
-    def getBaraja(self):
-        return self.baraja
-    
-    def cartaDescartar(self,carta):
-        return self.descartes.agregarCarta(carta)
-    
-    def cartaJugada(self,carta):
-        self.tablero.agregarCarta(carta)
-    
-    def cartaDescartadaTop(self):
-        '''Obtiene la ultima carta ingresada'''
-        return self.descartes.topCarta()
-    
-    def cartaDescartadaPop(self):
-        '''Elimina la ultima carta ingresada'''
-        self.descartes.popCarta()
