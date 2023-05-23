2ba8c7c7a63e489044da490f4968b10d26af800a
Ghost-Red <70116994+Ghost-Red@users.noreply.github.com>
2023-05-22 19:58:52 -0600

    P1_Redes_Cache

--- Arboretum Server/modelo/Jugador.py
+++ Arboretum Server/modelo/Jugador.py
@@ -1,35 +1,37 @@
-from Tablero import Tablero
+from modelo.Tablero import Tablero
 from Mazo import Mazo
 class Jugador:
     def __init__(self,nombre):
         self.nombre = nombre
-        self.tablero = Tablero(nombre)
-        self.descartes = Mazo(nombre)
-        self.baraja = Mazo(nombre)
-        self.tiposArbolPuntuable = []
+        self.cartas = []
         
-    def agregarArbolePuntuable(self,tiposArbol):
-        self.tiposArbolPuntuable.append(tiposArbol)
-        
-    def getNombre(self):
-        return self.nombre
-    
-    def setBaraja(self,cartas):
-        return self.baraja.setCartas(cartas)
-    
-    def getBaraja(self):
-        return self.baraja
-    
-    def cartaDescartar(self,carta):
-        return self.descartes.agregarCarta(carta)
-    
-    def cartaJugada(self,carta):
-        self.tablero.agregarCarta(carta)
-    
-    def cartaDescartadaTop(self):
-        '''Obtiene la ultima carta ingresada'''
-        return self.descartes.topCarta()
-    
-    def cartaDescartadaPop(self):
-        '''Elimina la ultima carta ingresada'''
-        self.descartes.popCarta()
+#        self.tablero = Tablero(nombre)
+#       self.descartes = Mazo(nombre)
+#       self.baraja = Mazo(nombre)
+#        self.tiposArbolPuntuable = []
+#        
+#    def agregarArbolePuntuable(self,tiposArbol):
+#        self.tiposArbolPuntuable.append(tiposArbol)
+#        
+#    def getNombre(self):
+#        return self.nombre
+#    
+#    def setBaraja(self,cartas):
+#        return self.baraja.setCartas(cartas)
+#    
+#    def getBaraja(self):
+#        return self.baraja
+#    
+#   def cartaDescartar(self,carta):
+#        return self.descartes.agregarCarta(carta)
+#    
+#    def cartaJugada(self,carta):
+#        self.tablero.agregarCarta(carta)
+#    
+#    def cartaDescartadaTop(self):
+#       '''Obtiene la ultima carta ingresada'''
+#        return self.descartes.topCarta()
+#    
+#    def cartaDescartadaPop(self):
+#        '''Elimina la ultima carta ingresada'''
+#        self.descartes.popCarta()
